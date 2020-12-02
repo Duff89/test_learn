fd5ed020cd8846f6c6c0e1024f9f6812397cf14a
Sergei <qwerty-sd@mail.ru>
2020-12-02 20:52:50 +0300

    add parametrize test of promo 0-9

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,22 +6,20 @@ from selenium.webdriver.common.by import By
 
 class ProductPage(BasePage):
     def button_add_to_cart_click(self):
-        self.button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
-        self.button.click()
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        button.click()
 
     def title_of_book_same(self):
-        self.title_of_book = self.browser.find_element(*ProductPageLocators.TITLE_OF_BOOK).text
-        self.title_of_book_in_cart = self.browser.find_element(
+        title_of_book = self.browser.find_element(*ProductPageLocators.TITLE_OF_BOOK).text
+        title_of_book_in_cart = self.browser.find_element(
             *ProductPageLocators.MESSAGE_ITEM_HAS_BEEN_ADD_TO_BASKET).text
-        assert self.title_of_book == self.title_of_book_in_cart, \
+        assert title_of_book == title_of_book_in_cart, \
             f'title book in cart: {self.title_of_book_in_cart}, ' \
             f'title book must be: {self.title_of_book}'
-        # проверка на одинаковое название в корзине
 
     def price_same(self):
-        self.price_of_book = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK).text
-        self.price_in_basket = self.browser.find_element(*ProductPageLocators.COMMON_PRICE_IN_BASKET).text
-        assert self.price_of_book in self.price_in_basket, \
+        price_of_book = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK).text
+        price_in_basket = self.browser.find_element(*ProductPageLocators.COMMON_PRICE_IN_BASKET).text
+        assert price_of_book in price_in_basket, \
             f'different price! now in basket: {self.price_in_basket}, ' \
             f'cost of book:{self.price_of_book}'
-        # проверка на одинаковую цену в корзине
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,24 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+import pytest
 
 
 def test_add_book_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/" \
-           "catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.button_add_to_cart_click()
+    page.solve_quiz_and_get_code()
+    page.title_of_book_same()
+    page.price_same()
+
+
+@pytest.mark.smoke
+@pytest.mark.parametrize('add_link',
+                         ['0', '1', '2', '3', '4', '5', '6', pytest.param("7", marks=pytest.mark.xfail), '8', '9'])
+def test_guest_can_add_product_to_basket(browser, add_link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{add_link}"
     page = ProductPage(browser, link)
     page.open()
     page.button_add_to_cart_click()
